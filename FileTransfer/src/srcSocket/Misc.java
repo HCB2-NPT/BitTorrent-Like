13e57e321bc89f3997bc3b2a194416516b6558e7
4azuo <hiroshi.kaze1994@gmail.com>
2016-10-07 15:15:32 +0700

    test

--- FileTransfer/src/srcSocket/Misc.java
+++ FileTransfer/src/srcSocket/Misc.java
@@ -19,18 +19,18 @@ public final class Misc {
 	
 	public static byte[] Long2Bytes(long i)
     {
-      byte[] result = new byte[8];
-
-      result[0] = (byte) (i >> 56);
-      result[1] = (byte) (i >> 48);
-      result[2] = (byte) (i >> 40);
-      result[3] = (byte) (i >> 32);
-      result[4] = (byte) (i >> 24);
-      result[5] = (byte) (i >> 16);
-      result[6] = (byte) (i >> 8);
-      result[7] = (byte) (i);
-
-      return result;
+	  byte[] result = new byte[8];
+	
+	  result[0] = (byte) (i >> 56);
+	  result[1] = (byte) (i >> 48);
+	  result[2] = (byte) (i >> 40);
+	  result[3] = (byte) (i >> 32);
+	  result[4] = (byte) (i >> 24);
+	  result[5] = (byte) (i >> 16);
+	  result[6] = (byte) (i >> 8);
+	  result[7] = (byte) (i);
+	
+	  return result;
     }
 	
 	public static void createTempFile(String fileName, long len) throws IOException{
