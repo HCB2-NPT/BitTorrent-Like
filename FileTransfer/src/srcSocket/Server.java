cb982760ea8ce067d2dac5173e214cd4216ae42a
4azuo <hiroshi.kaze1994@gmail.com>
2016-10-09 11:13:59 +0700

    complete project

--- FileTransfer/src/srcSocket/Server.java
+++ FileTransfer/src/srcSocket/Server.java
@@ -4,12 +4,19 @@ import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 import config.Constants;
+import helper.Debugger;
 import helper.MessageBox;
+import javafx.application.Platform;
 
 public class Server{
 	private DatagramSocket socket;
     private DatagramPacket packet;
     private boolean active = true;
+    private IServerEvent event;
+    
+    public Server(IServerEvent e){
+    	event = e;
+    }
     
     public void stop(){
     	active = false;
@@ -26,8 +33,15 @@ public class Server{
         }
         catch( Exception ex )
         {
-            System.out.println("Problem creating socket on port: " + Constants.PORT);
-        	//MessageBox.Show("Problem creating socket on port: " + Constants.PORT, "Shutdown...");
+            Debugger.log("Problem creating socket on port: " + Constants.PORT);
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                	MessageBox.Show("Problem creating socket on port: " + Constants.PORT, "Shutdown...");
+                	Platform.exit();
+					System.exit(0);
+                }
+            });
             return false;
         }
     	
@@ -65,27 +79,28 @@ public class Server{
                 System.arraycopy(receivedData, offset, ip, 0, ip.length);
                 Ip = new String(ip);
                 
-                offset += ip.length;
-                
-                data = new byte[receivedData.length - offset];
-                System.arraycopy(receivedData, offset, data, 0, data.length);
-                
-                switch(type){
-	                case skGlobals.dataType_Request:
-	                	ReceiveRequest(data, Ip);
-	            		break;
-	            	case skGlobals.dataType_Response:
-	            		ReceiveResponse(data, Ip);
-	            		break;
-	            	case skGlobals.dataType_SendSeedInfo:
-	            		ReceiveSeedInfo(data, Ip);
-	            		break;
-	            	case skGlobals.dataType_SendData_Continue:
-	            		ReceiveData(data, Ip, false);
-	            		break;
-	            	case skGlobals.dataType_SendData_End:
-	            		ReceiveData(data, Ip, true);
-	            		break;
+                if (Ip != Constants.getMyAddress()){
+                	offset += ip.length;
+	                data = new byte[receivedData.length - offset];
+	                System.arraycopy(receivedData, offset, data, 0, data.length);
+	                
+	                switch(type){
+		                case skGlobals.dataType_Request:
+		                	ReceiveRequest(data, Ip);
+		            		break;
+		            	case skGlobals.dataType_Response:
+		            		ReceiveResponse(data, Ip);
+		            		break;
+		            	case skGlobals.dataType_SendSeedInfo:
+		            		ReceiveSeedInfo(data, Ip);
+		            		break;
+		            	case skGlobals.dataType_SendData_Continue:
+		            		ReceiveData(data, Ip, false);
+		            		break;
+		            	case skGlobals.dataType_SendData_End:
+		            		ReceiveData(data, Ip, true);
+		            		break;
+	                }
                 }
             }
             catch (SocketException se)
@@ -106,26 +121,25 @@ public class Server{
 			@Override
 			public void run() {
     			try{
-			    	System.out.println("start receive-request");
-			    	System.out.println("from: " + from);
-			    	System.out.println("to: " + Constants.getMyAddress());
+			    	Debugger.log("start receive-request");
+			    	Debugger.log("from: " + from);
+			    	Debugger.log("to: " + Constants.getMyAddress());
 			    	
-			    	if (from != Constants.getMyAddress()){
-				    	byte[] fileNameLen = new byte[4];
-						System.arraycopy(data, 0, fileNameLen, 0, 4);
-						int len = ByteBuffer.wrap(fileNameLen).getInt();
-						
-						byte[] fileName = new byte[len];
-						System.arraycopy(data, 4, fileName, 0, len);
-						String name = new String(fileName, "UTF-8");
-						
-						File f = new File(Constants.FOLDER_SEED + name);
-						if (f.exists() && !f.isDirectory()){
-							Client.sendResponse(from, name);
-							System.out.println("send response success!");
-						}
-			    	}
-			    	System.out.println("end receive-request");
+			    	byte[] fileNameLen = new byte[4];
+					System.arraycopy(data, 0, fileNameLen, 0, 4);
+					int len = ByteBuffer.wrap(fileNameLen).getInt();
+					
+					byte[] fileName = new byte[len];
+					System.arraycopy(data, 4, fileName, 0, len);
+					String name = new String(fileName, "UTF-8");
+					
+					File f = new File(Constants.FOLDER_SEED + name);
+					if (f.exists() && !f.isDirectory()){
+						Client.sendResponse(from, name);
+						Debugger.log("send response success!");
+					}
+					
+			    	Debugger.log("end receive-request");
 		    	}
 		    	catch (Exception e){
 		    		//e.printStackTrace();
@@ -140,9 +154,9 @@ public class Server{
 			@Override
 			public void run() {
 				try{
-					System.out.println("start receive-response");
-					System.out.println("from: " + from);
-			    	System.out.println("to: " + Constants.getMyAddress());
+					Debugger.log("start receive-response");
+					Debugger.log("from: " + from);
+			    	Debugger.log("to: " + Constants.getMyAddress());
 			    	
 					byte[] fileNameLen = new byte[4];
 					System.arraycopy(data, 0, fileNameLen, 0, 4);
@@ -169,6 +183,8 @@ public class Server{
 						
 						//create empty file
 						Misc.createTempFile(name, fLen);
+						if (event != null)
+							event.ReceiveResponse();
 					}
 					
 					synchronized (dfi.readLocker){
@@ -178,7 +194,7 @@ public class Server{
 						dfi.Offset += lengthForSending;
 					}
 					
-					System.out.println("end receive-response");
+					Debugger.log("end receive-response");
 				}
 				catch (Exception e){
 					//e.printStackTrace();
@@ -194,9 +210,9 @@ public class Server{
 			@Override
 			public void run() {
 				try{
-					System.out.println("start receive:seed-info");
-					System.out.println("from: " + from);
-			    	System.out.println("to: " + Constants.getMyAddress());
+					Debugger.log("start receive:seed-info");
+					Debugger.log("from: " + from);
+			    	Debugger.log("to: " + Constants.getMyAddress());
 			    	
 					byte[] fileNameLen = new byte[4];
 					System.arraycopy(data, 0, fileNameLen, 0, 4);
@@ -214,8 +230,8 @@ public class Server{
 					System.arraycopy(data, 4 + len + 8, l, 0, 4);
 					int Length = ByteBuffer.wrap(l).getInt();
 					
-					System.out.println("end receive:seed-info");
-					System.out.println("start server:send-data");
+					Debugger.log("end receive:seed-info");
+					Debugger.log("start server:send-data");
 					
 					//read file-data
 					long off = Offset;
@@ -246,7 +262,7 @@ public class Server{
 						Thread.sleep(Constants.SENDING_DELAY);
 					}
 					
-					System.out.println("end server:send-data");
+					Debugger.log("end server:send-data");
 				}
 				catch (Exception e){
 					//e.printStackTrace();
@@ -261,9 +277,9 @@ public class Server{
 			@Override
 			public void run() {
 				try{
-					System.out.println("start receive-data");
-					System.out.println("from: " + from);
-			    	System.out.println("to: " + Constants.getMyAddress());
+					Debugger.log("start receive-data");
+					Debugger.log("from: " + from);
+			    	Debugger.log("to: " + Constants.getMyAddress());
 			    	
 					byte[] fileNameLen = new byte[4];
 					System.arraycopy(data, 0, fileNameLen, 0, 4);
@@ -297,23 +313,31 @@ public class Server{
 							    fh.close();
 							    
 							    dfi.Complete(Offset, Length);
+							    if (event != null)
+									event.ReceiveData(dfi);
+							    if (Debugger.IsEnable){
+								    for (SentData s : dfi.ListSentData) {
+										Debugger.log(s.Offset + " - " + s.Length);
+									}
+							    }
 								if (isEnd){
 									if (dfi.Offset < dfi.FileLength){
 										int lengthForSending = (int) Math.min(dfi.FileLength - dfi.Offset, dfi.MaxLengthForSending);
 										Client.sendSeedInfo(from, name, dfi.Offset, lengthForSending);
 										dfi.Offset += lengthForSending;
-										System.out.println("send seed-info, keep seeding another data...");
+										Debugger.log("send seed-info, keep seeding another data...");
 									}else{
 										SentData miss = dfi.getARangeLoss();
 										if (miss == null){
 											new File(Constants.FOLDER_SEED + Constants.PREFIX_EMPTY_FILE + name).renameTo(new File(Constants.FOLDER_SEED + name));
 											MappingFiles.getMap().remove(name);
-											//MessageBox.Show(name + " is downloaded!", "Notify");
-											System.out.println("Download complete!");
+											Debugger.log("Download complete!");
+											if (event != null)
+												event.DownloadCompleted(dfi);
 										}else{
 											int lengthForSending = (int) Math.min(miss.Length, dfi.MaxLengthForSending);
 											Client.sendSeedInfo(from, name, miss.Offset, lengthForSending);
-											System.out.println("Download missed data!");
+											Debugger.log("Download missed data!");
 										}
 									}
 								}
@@ -321,7 +345,7 @@ public class Server{
 						}
 					}
 					
-					System.out.println("end receive-data");
+					Debugger.log("end receive-data");
 				}
 				catch (Exception e){
 					//e.printStackTrace();
